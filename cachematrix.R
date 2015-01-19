9bec9f78c010837d56e8104036f5452c6cc1e280
zubair <djzubah@gmail.com>
2015-01-19 12:40:12 -0600

    final submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This combination of functions allows a matrix inverse to be stored
+## in cache in a class that encompasses the matrix.
 
-## Write a short comment describing this function
+## Class that encompasses a matrix by adding a inverse function to it
 
 makeCacheMatrix <- function(x = matrix()) {
-#testing first push
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(inverse) inv <<- inverse
+  getSolve <- function() inv
+  list(set = set, get = get, setSolve = setSolve, getSolve = getSolve)
+  
 }
 
 
-## Write a short comment describing this function
+## This function solves the encompassed matrix for an inverse.
+## If the matrix has already had its inverse found, it will be just be very quick
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getSolve()
+  if(is.null(inv)){
+    solution <- solve(x$get(),...)
+    x$setSolve(solution)
+    inv <- solution
+  } else{
+    print("Already solved, returning cached...")
+    return(inv)
+  }
+  inv
 }
